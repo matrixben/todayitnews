b28007d8de203ed45ef440900aa2c2abb7f75011
matrixben <ben354164737@qq.com>
2018-10-23 16:59:47 +0800

    new api for crawler

--- src/main/java/jason/luo/web/HelloController.java
+++ src/main/java/jason/luo/web/HelloController.java
@@ -3,6 +3,12 @@ package jason.luo.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
 @RestController
 public class HelloController {
     @RequestMapping("/hi")
@@ -10,4 +16,25 @@ public class HelloController {
         return "This project is going to gather all IT news' title and url " +
                 "to one page for me to view.";
     }
+    
+    @RequestMapping("/solidot")
+	public void getSolidotNews() throws Exception {
+		String crawlStorageFolder = ".";
+		int numberOfCrawlers = 1;
+		CrawlConfig config = new CrawlConfig();
+		config.setCrawlStorageFolder(crawlStorageFolder);
+		config.setPolitenessDelay(1000);
+		config.setMaxDepthOfCrawling(0);
+		config.setMaxPagesToFetch(20);
+		
+		PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        robotstxtConfig.setEnabled(false);
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        controller.addSeed("https://www.solidot.org");
+        
+        controller.start(MyCrawler.class, numberOfCrawlers);
+	}
 }
